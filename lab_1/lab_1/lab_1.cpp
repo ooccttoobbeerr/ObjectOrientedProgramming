4edb03498079d5865e8df53798a0ab2bc208f57a
ooccttoobbeerr <oooccctttooobbbeeerrr@gmail.com>
2016-02-17 17:57:27 +0300

    копирование одного файла в другой

---
+++ lab_1/lab_1/lab_1.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+#include <fstream>
+#include <stdio.h>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+void CopyInputFileToOutputFile(istream & inputFile, ostream & outputFile)
+{
+	string str;
+	while (getline(inputFile, str))
+	{
+		outputFile << str << endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	if (argc != 3)
+	{
+		cout << "You have entered an false number of arguments!\nInput Example: lab_1.exe input.txt output.txt" << endl;
+	}
+	else
+	{
+		ifstream inputFile(argv[1]);
+		ofstream outputFile(argv[2]);
+		if (inputFile.is_open() && outputFile.is_open())
+		{
+			CopyInputFileToOutputFile(inputFile, outputFile);
+		}
+		else
+		{
+			cout << "Error opening file!" << endl;
+		}
+		inputFile.close();
+		outputFile.close();
+	}
+	return 0;
+}
+
+
+
